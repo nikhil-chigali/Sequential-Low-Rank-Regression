508ec48c93b8ddfd1eb5e9fd2f8f98c5ac97612b
nikhil-chigali <nikhil.chigali@gmail.com>
2024-07-25 14:11:03 +0530

    feat: Log matrix factorization experiment results to CSV file

--- utils/utils.py
+++ utils/utils.py
@@ -1,26 +1,32 @@
 import os
 
 
-def log_to_csv(exp_name, filename, rel_errors, errors):
+def log_to_csv(filename, exp_name, errors, args):
     """
-    Log the relative errors and errors (MSE, Orth_A, Orth_B) of A and B to a CSV file
+    Log the args, relative errors and errors (MSE, Orth_A, Orth_B) of A and B to a CSV file
     """
     if not os.path.isfile(filename):
         with open(filename, "w", encoding="utf-8") as f:
             f.write(
-                "Experiment,Rel_Error_A,Rel_Error_B,MSE_loss,Orth_loss_A,Orth_loss_B\n"
+                "Experiment,LearningRate_A,LearningRate_B,Orth_steps_per_iter,Rel_Error_W,MSE_loss,Orth_loss_A,Orth_loss_B\n"
             )
+
+    rel_error = errors["rel_error_W"][-1] if "rel_error_W" in errors else "NAN"
+    mse = errors["mse"][-1] if "mse" in errors else "NAN"
+    tau = args.tau if "orth_A" in errors else "NAN"
+    orth_A = errors["orth_A"][-1] if "orth_A" in errors else "NAN"
+    orth_B = errors["orth_B"][-1] if "orth_B" in errors else "NAN"
+
+    print("Experiment:", exp_name)
+    print("Learning Rate A:", args.eta_A)
+    print("Learning Rate B:", args.eta_B)
+    print("Orthogonalization Steps per Iteration:", tau)
+    print("Relative Error of (W, W*):", rel_error)
+    print("MSE Loss:", mse)
+    print("Orthogonal Constraint Loss A:", orth_A)
+    print("Orthogonal Constraint Loss B:", orth_B)
 
     with open(filename, "a", encoding="utf-8") as f:
         f.write(
-            f"{exp_name},{rel_errors['A'][-1]},{rel_errors['B'][-1]},{errors['MSE'][-1]},{errors['orth_A'][-1]},{errors['orth_B'][-1]}\n"
+            f"{exp_name},{args.eta_A},{args.eta_B},{tau},{rel_error},{mse},{orth_A},{orth_B}\n"
         )
-
-    print("Relative Error of A: ", rel_errors["A"][-1])
-    print("Relative Error of B: ", rel_errors["B"][-1])
-    print("MSE Loss: ", errors["MSE"][-1])
-    print(
-        "Orthogonal Constraint Loss (A, B): ",
-        errors["orth_A"][-1],
-        errors["orth_B"][-1],
-    )
--- utils/visualize.py
+++ utils/visualize.py
@@ -1,63 +1,62 @@
 from matplotlib import pyplot as plt
 
 
-def plot_all_errors(rel_errors, errors):
+def plot_all_errors(exp_name: str, errors: dict) -> None:
     """
-    Plot the relative errors and errors (Orth_A, Orth_B) of A and B in a grid layout
+    Plot the relative error, MSE and Orthogonalization error (Orth_A, Orth_B) of A and B in a grid layout
+
+    Parameters:
+    exp_name (str): Name of the experiment
+    errors (dict): Dictionary containing the errors of the model
+
+    Returns:
+    None
     """
-    _, ax = plt.subplots(2, 2, figsize=(12, 10))
+    # Check if the orthogonalization errors are present
+    plot_orth = "orth_A" in errors and "orth_B" in errors
 
-    # Relative errors of A and B
-    ax[0, 0].plot(
-        range(1, len(rel_errors["A"]) + 1),
-        rel_errors["A"],
-        label="||A-A*||/||A*||",
-        c="r",
-    )
-    ax[0, 0].set_xlabel("Iterations")
-    ax[0, 0].set_ylabel("Relative Error")
-    ax[0, 0].set_title("Convergence of `FactorizedGD (A)` to `A*`")
-    ax[0, 0].legend()
-    ax[0, 0].grid()
+    if plot_orth:
+        fig, axs = plt.subplots(2, 2, figsize=(15, 10))
+        ax1, ax2 = axs[0]
+    else:
+        fig, axs = plt.subplots(2, 1, figsize=(15, 10))
+        ax1, ax2 = axs
 
-    # Relative errors of A and B
-    ax[0, 1].plot(
-        range(1, len(rel_errors["B"]) + 1),
-        rel_errors["B"],
-        label="||B-B*||/||B*||",
-        c="g",
-    )
-    ax[0, 1].set_xlabel("Iterations")
-    ax[0, 1].set_ylabel("Relative Error")
-    ax[0, 1].set_title("Convergence of `FactorizedGD (B)` to `B*`")
-    ax[0, 1].legend()
-    ax[0, 1].grid()
+    fig.suptitle(exp_name)
 
-    # Errors of A and B
-    ax[1, 0].plot(
-        range(1, len(errors["orth_A"]) + 1),
-        errors["orth_A"],
-        label="Orthogonal Loss A",
-        c="b",
-    )
-    ax[1, 0].set_xlabel("Iterations")
-    ax[1, 0].set_ylabel("Loss")
-    ax[1, 0].set_title("Orthogonal Loss of `A`")
-    ax[1, 0].legend()
-    ax[1, 0].grid()
+    # Plot the relative error of W
+    ax1.plot(errors["rel_error_W"], label="Relative Error of W")
+    ax1.set_ylim(top=1, bottom=0)
+    ax1.set_title("Relative Error of (W,W*)")
+    ax1.set_xlabel("Iterations")
+    ax1.set_ylabel("Relative Error")
+    ax1.grid()
+    ax1.legend()
 
-    # Errors of A and B
-    ax[1, 1].plot(
-        range(1, len(errors["orth_B"]) + 1),
-        errors["orth_B"],
-        label="Orthogonal Loss B",
-        c="m",
-    )
-    ax[1, 1].set_xlabel("Iterations")
-    ax[1, 1].set_ylabel("Loss")
-    ax[1, 1].set_title("Orthogonal Loss of `B`")
-    ax[1, 1].legend()
-    ax[1, 1].grid()
+    # Plot the MSE loss
+    ax2.plot(errors["mse"], label="MSE Loss")
+    # ax2.set_ylim(top=5000, bottom=0)
+    ax2.set_title("MSE Loss")
+    ax2.set_xlabel("Iterations")
+    ax2.set_ylabel("MSE Loss")
+    ax2.grid()
+    ax2.legend()
+
+    if plot_orth:
+        # Plot the orthogonalization errors
+        axs[1, 0].plot(errors["orth_A"], label="Orthogonal Constraint Loss A")
+        axs[1, 0].set_title("Orthogonal Constraint Loss A")
+        axs[1, 0].set_xlabel("Iterations")
+        axs[1, 0].set_ylabel("Orthogonal Constraint Loss")
+        axs[1, 0].grid()
+        axs[1, 0].legend()
+
+        axs[1, 1].plot(errors["orth_B"], label="Orthogonal Constraint Loss B")
+        axs[1, 1].set_title("Orthogonal Constraint Loss B")
+        axs[1, 1].set_xlabel("Iterations")
+        axs[1, 1].set_ylabel("Orthogonal Constraint Loss")
+        axs[1, 1].grid()
+        axs[1, 1].legend()
 
     plt.tight_layout()
     plt.show()
